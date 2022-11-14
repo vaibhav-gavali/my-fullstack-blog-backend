13bbb6a132e5f1eac3ce6e340d330a0c9a63b6b9
Vaibhav Gavali <vaibhavgavali92@gmail.com>
2022-11-14 12:52:39 +0530

    Firebase auth applicated for APIs

--- src/server.js
+++ src/server.js
@@ -1,52 +1,93 @@
 import express from 'express';
+import fs from 'fs';
+import admin from 'firebase-admin';
 import { db, connectToDb } from './db.js';
 
 // localhost:3000/articles/learn-node
 // PUT /articles/learn-react/upvote
 
-const app = express();
+const credentials = JSON.parse(fs.readFileSync('./credentials.json'));
+admin.initializeApp({
+  credential: admin.credential.cert(credentials),
+});
 
+const app = express();
 app.use(express.json()); // If we want req.body to work in express js
+
+app.use(async (req, res, next) => {
+  const { authtoken } = req.headers;
+
+  if (authtoken) {
+    try {
+      req.user = await admin.auth().verifyIdToken(authtoken);
+    } catch (e) {
+      return res.sendStatus(400); // Bad Request
+    }
+  }
+
+  req.user = req.user || {};
+  next();
+});
 
 app.get('/api/articles/:name', async (req, res) => {
   const { name } = req.params;
+  const { uid } = req.user;
 
   const article = await db.collection('articles').findOne({ name });
 
   if (article) {
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpvote = uid && !upvoteIds.includes(uid);
     res.json(article);
   } else {
     res.sendStatus(404).send('Article Not Found');
   }
 });
 
+// Middleware for Upvote and Comments
+app.use((req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401); //User not allowed
+  }
+});
+
 app.put('/api/articles/:name/upvote', async (req, res) => {
   const { name } = req.params;
-
-  await db.collection('articles').updateOne(
-    { name },
-    {
-      $inc: { upvotes: 1 },
-    }
-  );
+  const { uid } = req.user;
 
   const article = await db.collection('articles').findOne({ name });
 
   if (article) {
-    res.json(article);
+    const upvoteIds = article.upvoteIds || [];
+    const canUpvote = uid && !upvoteIds.includes(uid);
+
+    if (canUpvote) {
+      await db.collection('articles').updateOne(
+        { name },
+        {
+          $inc: { upvotes: 1 },
+          $push: { upvoteIds: uid },
+        }
+      );
+    }
+    const updatedArticle = await db.collection('articles').findOne({ name });
+    res.json(updatedArticle);
   } else {
     res.send("That article doesn't exist");
   }
 });
 
 app.post('/api/articles/:name/comments', async (req, res) => {
-  const { postedBy, text } = req.body;
+  const { text } = req.body;
   const { name } = req.params;
+  const { email } = req.user;
 
   await db.collection('articles').updateOne(
     { name },
     {
-      $push: { comments: { postedBy, text } },
+      $push: { comments: { postedBy: email, text } },
     }
   );
 
